d236d4fcd62f2cb8cf036e17e73998e1d2b6137c
Choi, Hyesun <gptjs409@naver.com>
2019-05-23 16:34:59 +0900

    [10172] 개 #5

---
+++ src/y2019/m05/d23/n10172/Main.java
@@ -0,0 +1,11 @@
+package y2019.m05.d23.n10172;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+		
+	}
+
+}
