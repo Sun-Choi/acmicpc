79f1260ddf2e06bef444cc2086354f902914f157
Choi, Hyesun <gptjs409@naver.com>
2019-05-23 14:09:54 +0900

    #1 [1000] A+B

---
+++ src/y2019/m05/d23/n1000/Main.java
@@ -0,0 +1,23 @@
+package y2019.m05.d23.n1000;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y, z;
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		sc.close();
+		
+		z = x + y;
+		
+		System.out.println(z);
+	}
+
+}
